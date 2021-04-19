daa9c488b1e8b6449ce15dff63b660a33a05f008
toadette <toadettek@outlook.com>
2021-04-19 18:12:50 +0800

    2021419

--- pages/home/home.js
+++ pages/home/home.js
@@ -20,6 +20,7 @@ Page({
       this.setData({
         'bannerArr':res.items,
       })
+      // console.log(res)
     });
 
     home.getThemeData((res)=>{
@@ -30,14 +31,28 @@ Page({
     });
 
     home.getProductsData((data)=>{
-      console.log(data)
+      // console.log(data)
       this.setData({
         productsArr: data
       })
     })
   
   },//处理异步结果接受的方法
-
+ 
+  onProductItemTap:function(event){
+    //从base这个基类把getDataSet引入并调用，而base从Home里引入了
+    var id = home.getDataSet(event,'id');  //id是自己写的变量名，是data-id横线后面的部分，可以换成别的，所以要加单引号作为string
+    wx.navigateTo({
+      url: "../product/product?id="+id,
+    });
+  },
+  onThemeItemTap:function(event){
+    var id = home.getDataSet(event,'id');
+    var name = home.getDataSet(event,'name');
+    wx.navigateTo({
+      url: "../theme/theme?id="+id + '&name=' + name,
+    });
+  }
 
 
 })
