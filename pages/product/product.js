3729a135e28a04191e7bd55634afe4a3273517f2
toadette <toadettek@outlook.com>
2021-04-20 16:54:37 +0800

    2021420下午

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,11 +1,36 @@
-Page({
-/*页面初始数据*/
-data:{
-},
-/*生命周期函数--监听页面加载*/
-onLoad:function(options){
-    var id= options.id;
-    console.log(id);
-},
+import { Product } from 'product-model.js';
+var product = new Product();
 
+Page({
+    /*页面初始数据*/
+    data: {
+        id: null,
+        countsArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        productCount:1
+    },
+    /*生命周期函数--监听页面加载*/
+    onLoad: function (options) {
+        var id = options.id;
+        this.data.id = id;
+
+        this._loadData();
+    },
+
+    _loadData: function () {
+        product.getDetailInfo(this.data.id, (data) => {
+            this.setData({
+                product: data
+            });
+        });
+    },
+
+    bindPickerChange: function (event) {
+        var index = event.detail.value;
+        var selectedCount = this.data.countsArray[index];
+        this.setData({
+            productCount:selectedCount
+        })
+    }
 })
+
+
--- pages/theme/theme-model.js
+++ pages/theme/theme-model.js
@@ -18,4 +18,5 @@ class Theme extends Base{
     }
 }
 
+export {Theme}
 
